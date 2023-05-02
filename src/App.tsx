628dd5b58f58fb4bc5e73e80af8519a5a6d7c3b1
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-05-02 15:29:06 -0600

    wip on gsb

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css'
 import { RuxContainer } from "@astrouxds/react"
+import GlobalStatusBar from './Components/GlobalStatusBar';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStatusBar/>
       <RuxContainer>
-      Hello World
       </RuxContainer>
 
     </div>
