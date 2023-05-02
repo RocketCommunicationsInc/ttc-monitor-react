2e1414abbac809b186330d42d29927a2de3c7488
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-05-02 10:03:10 -0700

    adding astro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css'
+import { RuxContainer } from "@astrouxds/react"
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <RuxContainer>
       Hello World
+      </RuxContainer>
+
     </div>
   );
 }
