9b5a0dfb8b634c3a6594bd6468c24b2b605dc394
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-05-02 12:49:47 -0700

    creates constellation sub components

--- src/Components/Constellation.tsx
+++ src/Components/Constellation.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import { useState } from "react"
 import { RuxContainer, RuxSegmentedButton } from "@astrouxds/react"
+import ConstellationList from "./ListHeader"
+import ConstellationTimeline from "./ConstellationTimeline"
+import { RuxSegmentedButtonCustomEvent } from "@astrouxds/astro-web-components/dist/types/components"
 
 const Constellation = () => {
+  const [content, setContent] = useState(<ConstellationList />)
+
+  const handleButton = (e: RuxSegmentedButtonCustomEvent<string>) => {
+    console.log(e.detail)
+    if (e.detail === "List") {
+      setContent(<ConstellationList />)
+    } else { 
+      setContent(<ConstellationTimeline />)
+    }
+  }
+
   return (
     <RuxContainer className="constellation">
-      <div slot="header">
-        <span>Constellation</span>
-        <RuxSegmentedButton>
+      <div slot="header" style={{display: "flex"}}>
+        Constellation
+        <RuxSegmentedButton
+          style={{ marginLeft: "auto"}}
+          data=
+          {[
+            { label: "List"},
+            { label: "Timeline"},
+          ]}
+          onRuxchange={handleButton}
+        >
         </RuxSegmentedButton>
       </div>
-        <div slot="footer">
+      <div className="constellation-content">
+        {content}
       </div>
     </RuxContainer>
   )
