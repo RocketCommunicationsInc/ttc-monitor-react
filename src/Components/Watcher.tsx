d82068d1cbf86afaa6757e95e36d3086fb638fb2
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-05-04 15:15:59 -0700

    moves mneumonic popup to separate component

--- src/Components/Watcher.tsx
+++ src/Components/Watcher.tsx
@@ -8,37 +8,14 @@ import {
   RuxTableRow,
   RuxTableCell,
   RuxTableBody,
-  RuxPopUp,
-  RuxMenu,
-  RuxCheckbox,
-  RuxIcon,
 } from "@astrouxds/react";
 import type { rowDataObject } from "../Types/types";
 import LineChart from "./LineChart";
+import MneumonicPopUp from "./MneumonicPopUp";
 
 const styles = {
   container: {
     display: "flex",
-  },
-  popUpMenuDiv: {
-    paddingRight: "3rem",
-    paddingLeft: "1rem",
-    marginBlock: ".5rem",
-  },
-  volts: {
-    marginLeft: "4rem",
-    fontWeight: "var(--font-weights-bold)",
-  },
-  altitudeLink: {
-    marginLeft: "1.5rem",
-  },
-  boldPopUpWords: {
-    fontWeight: "var(--font-weights-bold)",
-  },
-  popUpValue: {
-    textDecoration: "underline",
-    textDecorationStyle: "dashed",
-    color: "var(--color-palette-brightblue-300)",
   },
 };
 
@@ -50,18 +27,18 @@ const watcherDataItem = {
   Actual: "Full",
 };
 
-const fixtureData = Array(5).fill(watcherDataItem);
+const fixtureData = Array(10).fill(watcherDataItem);
 
 const Watcher = () => {
   return (
-    <RuxContainer className="watcher">
-      <div slot="header" style={styles.container}>
-        Watcher
-      </div>
-      <div slot="toolbar" style={styles.container}>
-        IRON 4090
-      </div>
-      <div className="watcher-body">
+    <div className="watcher">
+      <RuxContainer>
+        <div slot="header" style={styles.container}>
+          Watcher
+        </div>
+        <div slot="toolbar" style={styles.container}>
+          IRON 4090
+        </div>
         <RuxTable>
           <RuxTableHeader>
             <RuxTableHeaderRow>
@@ -81,40 +58,9 @@ const Watcher = () => {
                       <RuxStatus status={dataObj.status} />
                     </RuxTableCell>
                   ) : key === "Mneumonic" ? (
-                    <RuxPopUp placement="right">
-                      <RuxMenu>
-                        <div style={styles.popUpMenuDiv}>
-                          <span style={styles.boldPopUpWords}>PWBVTLM</span>
-                          <p>
-                            <div>
-                              Value
-                              <span style={styles.volts}>24.2 Volts</span>
-                            </div>
-                            <div>
-                              Subsystem
-                              <span style={styles.altitudeLink}>
-                                <a
-                                  href="https://ttc-investigate.astrouxds.com/?system=Attitude"
-                                  target="_blank"
-                                  rel="noreferrer"
-                                >
-                                  Altitude
-                                </a>
-                                <RuxIcon size="1rem" icon="launch" />
-                              </span>
-                            </div>
-                          </p>
-                          <RuxCheckbox>
-                            <span style={styles.boldPopUpWords}>
-                              Add to Watcher
-                            </span>
-                          </RuxCheckbox>
-                        </div>
-                      </RuxMenu>
-                      <RuxTableCell style={styles.popUpValue} slot="trigger">
-                        {value}
-                      </RuxTableCell>
-                    </RuxPopUp>
+                    <RuxTableCell>
+                      <MneumonicPopUp triggerValue={value} />
+                    </RuxTableCell>
                   ) : (
                     <RuxTableCell>{value}</RuxTableCell>
                   )
@@ -123,9 +69,9 @@ const Watcher = () => {
             ))}
           </RuxTableBody>
         </RuxTable>
-        <LineChart />
-      </div>
-    </RuxContainer>
+      </RuxContainer>
+      <LineChart />
+    </div>
   );
 };
 
