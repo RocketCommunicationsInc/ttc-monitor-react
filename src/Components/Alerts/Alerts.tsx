28ba5e89be64336e11c1999e73ac34385d4661a7
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-05-04 16:01:12 -0600

    working on save footer buttons, working on notification banner

--- src/Components/Alerts/Alerts.tsx
+++ src/Components/Alerts/Alerts.tsx
@@ -40,34 +40,37 @@ const styles = {
     textAlign: "center" as "center",
     marginRight: "auto",
   },
-  footer: {
-    display: "flex",
-    justifyContent: "center",
-    borderTop: "1px solid var(--logHeaderBackgroundColor, rgb(20, 32, 44))",
-    boxShadow: " 0 -0.5rem 1.25rem rgba(0, 0, 0, 0.25)",
-    width: "28.75rem",
-    height: "3rem",
-    padding: "2rem",
-    position: "absolute" as "absolute",
-    bottom: -2,
-    backgroundColor: "#1B2D3E",
+  notificationBanner: {
+    flexGrow: 1,
+    position: "sticky" as "sticky",
+    top: 0,
+    left: 0,
+    alignContent: "center",
+    backgroundColor: "var(--colorPrimaryDarken1, rgb(0, 68, 107))",
+    color: "var(--colorPrimaryLighten3, rgb(211, 234, 255))",
+    padding: "0.5rem",
+    textAlign: "center",
+    width: "100%",
   },
 };
 
 const Alerts = () => {
 
-  const [notificationBanner, setNotifcationBanner] = useState(false)
+  // const [notificationBanner, setNotifcationBanner] = useState(false);
+  // const notification: any = document.getElementByClassName(".notificationBanner");
 
-  const notification: any = document.getElementById("#notification")
-
-const notificationHandler = () => {
-    // notification?.addEventListener('ruxclosed', () => {
-    //     }
-    // )
-   setNotifcationBanner(!notificationBanner)
-}
-
-
+  const notificationHandler = () => {
+    // return (
+    //   <RuxNotification style={styles.notificationBanner} className="notificationBanner">
+    //     You have one or more filters selected.
+    //     <br />
+    //     <RuxButton secondary borderless>
+    //       Clear filters
+    //     </RuxButton>
+    //     to display all alerts.
+    //   </RuxNotification>
+    // );
+  };
 
   return (
     <RuxContainer className="alerts" style={styles.container}>
@@ -78,33 +81,35 @@ const notificationHandler = () => {
         <div style={styles.selectMenusDiv}>
           <RuxSelect size="small" label="Severity" style={styles.select1}>
             <RuxOption label="All" value="" />
-            <RuxOption label="Critical" value="" onClick={notificationHandler}/>
-            <RuxOption label="Caution" value=""/>
-            <RuxOption label="Serious" value=""/>
-          
-               <RuxNotification id="notification">
-              You have one or more filters selected.
-              <br />
-              <RuxButton secondary borderless>
-                Clear filters
-              </RuxButton>
-              to display all alerts.
-            </RuxNotification>
-         
+            <RuxOption
+              label="Critical"
+              value=""
+              onClick={notificationHandler}
+            />
+            <RuxOption label="Caution" value="" onClick={notificationHandler} />
+            <RuxOption label="Serious" value="" onClick={notificationHandler} />
           </RuxSelect>
           <RuxSelect size="small" label="Category" style={styles.select2}>
-            <RuxOption label="All" value=""/>
-            <RuxOption label="Hardware" value=""/>
-            <RuxOption label="Software" value=""/>
-            <RuxOption label="Spacecraft" value=""/>
+            <RuxOption label="All" value="" />
+            <RuxOption
+              label="Hardware"
+              value=""
+              onClick={notificationHandler}
+            />
+            <RuxOption
+              label="Software"
+              value=""
+              onClick={notificationHandler}
+            />
+            <RuxOption
+              label="Spacecraft"
+              value=""
+              onClick={notificationHandler}
+            />
           </RuxSelect>
         </div>
       </div>
       <AlertsList />
-      <div style={styles.footer}>
-        <RuxButton style={{ marginRight: "1rem" }}>Ackowledge</RuxButton>
-        <RuxButton>Dismiss</RuxButton>
-      </div>
     </RuxContainer>
   );
 };
