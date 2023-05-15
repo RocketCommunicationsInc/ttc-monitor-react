ef754dc1c3a866c47a86555a52e0207b5c642935
Joel Perez <joel@joelperez.dev>
2023-05-15 14:27:54 -0600

    add drawer to table

--- src/Components/Constellation/Constellation.tsx
+++ src/Components/Constellation/Constellation.tsx
@@ -12,6 +12,8 @@ import {
   RuxSliderCustomEvent,
 } from "@astrouxds/astro-web-components/dist/types/components";
 import useContacts from "../../hooks/useContacts";
+import ContactDrawer from "../ContactDrawer/ContactDrawer";
+import { Contact } from "../../Types";
 
 const styles = {
   slider: {
@@ -24,6 +26,8 @@ const styles = {
 const Constellation = () => {
   const [content, setContent] = useState("List");
   const [zoomLevel, setZoomLevel] = useState(1);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [drawerContact, setDrawerContact] = useState<Contact | null>(null);
 
   const { contacts, contactIds, initialize } = useContacts();
 
@@ -35,42 +39,58 @@ const Constellation = () => {
     setZoomLevel(e.target.value);
   };
 
+  const toggleDrawer = (id?: string) => {
+    if (id) setDrawerContact(contacts[id]);
+    setDrawerOpen((prevState) => !prevState);
+  };
+
   useEffect(() => {
     initialize();
   }, [initialize]);
 
   return (
-    <RuxContainer className="constellation">
-      <div slot="header" style={{ display: "flex" }}>
-        Constellation
-        {content === "Timeline" ? (
-          <div style={styles.slider}>
-            <RuxIcon icon="zoom-out" size="extra-small" />
-            <RuxSlider
-              value={zoomLevel}
-              onRuxinput={handleSliderChange}
-              min={0}
-              max={10}
-            ></RuxSlider>
-            <RuxIcon icon="zoom-in" size="20px" />
-          </div>
-        ) : null}
-        <RuxSegmentedButton
-          style={
-            content === "Timeline"
-              ? { marginLeft: "1rem" }
-              : { marginLeft: "auto" }
-          }
-          data={[{ label: "List" }, { label: "Timeline" }]}
-          onRuxchange={handleButton}
-        ></RuxSegmentedButton>
-      </div>
-      {content === "List" && contactIds.length ? (
-        <ConstellationList contacts={contacts} contactIds={contactIds} />
-      ) : (
-        <ConstellationTimeline zoomLevel={zoomLevel} />
-      )}
-    </RuxContainer>
+    <>
+      <RuxContainer className="constellation">
+        <div slot="header" style={{ display: "flex" }}>
+          Constellation
+          {content === "Timeline" ? (
+            <div style={styles.slider}>
+              <RuxIcon icon="zoom-out" size="extra-small" />
+              <RuxSlider
+                value={zoomLevel}
+                onRuxinput={handleSliderChange}
+                min={0}
+                max={10}
+              ></RuxSlider>
+              <RuxIcon icon="zoom-in" size="20px" />
+            </div>
+          ) : null}
+          <RuxSegmentedButton
+            style={
+              content === "Timeline"
+                ? { marginLeft: "1rem" }
+                : { marginLeft: "auto" }
+            }
+            data={[{ label: "List" }, { label: "Timeline" }]}
+            onRuxchange={handleButton}
+          ></RuxSegmentedButton>
+        </div>
+        {content === "List" && contactIds.length ? (
+          <ConstellationList
+            contacts={contacts}
+            contactIds={contactIds}
+            toggleDrawer={toggleDrawer}
+          />
+        ) : (
+          <ConstellationTimeline zoomLevel={zoomLevel} />
+        )}
+      </RuxContainer>
+      <ContactDrawer
+        open={drawerOpen}
+        toggle={toggleDrawer}
+        contact={drawerContact}
+      />
+    </>
   );
 };
 
