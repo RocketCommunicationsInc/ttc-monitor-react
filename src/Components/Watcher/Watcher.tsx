445d44e808bad948d7d913800c999735934971ea
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-05-17 12:13:02 -0700

    updated mnemonic table to be tree node

--- src/Components/Watcher/Watcher.tsx
+++ src/Components/Watcher/Watcher.tsx
@@ -8,6 +8,8 @@ import {
   RuxTableRow,
   RuxTableCell,
   RuxTableBody,
+  RuxTree,
+  RuxTreeNode,
 } from "@astrouxds/react";
 import type { rowDataObject, Status } from "../../Types";
 import LineChart from "./LineChart";
@@ -36,43 +38,50 @@ const Watcher = () => {
         <div slot="header" style={styles.container}>
           Watcher
         </div>
-        <div slot="toolbar" style={styles.container}>
-          IRON 4090
-        </div>
-        <RuxTable>
-          <RuxTableHeader>
-            <RuxTableHeaderRow>
-              <RuxTableHeaderCell>
-                {/* placeholder for status icon column */}
-              </RuxTableHeaderCell>
-              <RuxTableHeaderCell>Mnemonic</RuxTableHeaderCell>
-              <RuxTableHeaderCell>Unit</RuxTableHeaderCell>
-              <RuxTableHeaderCell>Threshold</RuxTableHeaderCell>
-              <RuxTableHeaderCell>Actual</RuxTableHeaderCell>
-            </RuxTableHeaderRow>
-          </RuxTableHeader>
-          <RuxTableBody>
-            {fixtureData.map((dataObj: rowDataObject) => (
-              <RuxTableRow key={dataObj.key}>
-                {Object.entries(dataObj).map(([key, value]) =>
-                  key === "status" ? (
-                    <RuxTableCell>
-                      <RuxStatus status={dataObj.status as Status} />
-                    </RuxTableCell>
-                  ) : key === "mnemonic" ? (
-                    <RuxTableCell>
-                      <MnemonicPopUp triggerValue={value} />
-                    </RuxTableCell>
-                  ) : (
-                    <RuxTableCell style={{ textAlign: "right" }}>
-                      {value}
-                    </RuxTableCell>
-                  )
-                )}
-              </RuxTableRow>
-            ))}
-          </RuxTableBody>
-        </RuxTable>
+        {/* <div slot="toolbar" style={styles.container}>
+
+        </div> */}
+        <RuxTree>
+          <RuxTreeNode>
+            IRON 4090
+            <RuxTreeNode slot="node">
+            <RuxTable>
+              <RuxTableHeader>
+                <RuxTableHeaderRow>
+                  <RuxTableHeaderCell>
+                    {/* placeholder for status icon column */}
+                  </RuxTableHeaderCell>
+                  <RuxTableHeaderCell>Mnemonic</RuxTableHeaderCell>
+                  <RuxTableHeaderCell>Unit</RuxTableHeaderCell>
+                  <RuxTableHeaderCell>Threshold</RuxTableHeaderCell>
+                  <RuxTableHeaderCell>Actual</RuxTableHeaderCell>
+                </RuxTableHeaderRow>
+              </RuxTableHeader>
+              <RuxTableBody>
+                {fixtureData.map((dataObj: rowDataObject) => (
+                  <RuxTableRow key={dataObj.key}>
+                    {Object.entries(dataObj).map(([key, value]) =>
+                      key === "status" ? (
+                        <RuxTableCell>
+                          <RuxStatus status={dataObj.status as Status} />
+                        </RuxTableCell>
+                      ) : key === "mnemonic" ? (
+                        <RuxTableCell>
+                          <MnemonicPopUp triggerValue={value} />
+                        </RuxTableCell>
+                      ) : (
+                        <RuxTableCell style={{ textAlign: "right" }}>
+                          {value}
+                        </RuxTableCell>
+                      )
+                    )}
+                  </RuxTableRow>
+                ))}
+              </RuxTableBody>
+              </RuxTable>
+              </RuxTreeNode>
+          </RuxTreeNode>
+        </RuxTree>
       </RuxContainer>
       <div className="canvas-wrapper">
         <LineChart subtitle={watcherDataItem.mnemonic} />
