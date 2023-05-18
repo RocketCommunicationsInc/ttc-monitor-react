3caf2bd7ec0c6338cb76492213ba4c9159ec8926
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-05-18 10:28:22 -0600

    problem with footer is container it's in- not footer itself

--- src/Components/Alerts/Alerts.tsx
+++ src/Components/Alerts/Alerts.tsx
@@ -61,7 +61,7 @@ const Alerts = () => {
   };
 
   return (
-    <RuxContainer className="alerts" style={{paddingBottom: 0}}>
+    <RuxContainer className="alerts">
       <div slot="header" style={styles.header}>
         <div style={styles.activeAlerts}>
           <span style={styles.alertsNum}>{alertIds.length}</span> Active Alerts
--- src/Components/Alerts/AlertsFooter.tsx
+++ src/Components/Alerts/AlertsFooter.tsx
@@ -7,15 +7,19 @@ const styles = {
     justifyContent: "center",
     borderTop: "1px solid var(--logHeaderBackgroundColor, rgb(20, 32, 44))",
     boxShadow: " 0 -0.5rem 1.25rem rgba(0, 0, 0, 0.25)",
-    paddingTop: "2rem",
+    paddingTop: "1.5rem",
+  },
+  dismissBtn: {
+    marginRight: "1rem",
+    paddingBottom: "1.5rem",
   },
 };
 
-type PropTypes ={
-  slot: string
-}
+type PropTypes = {
+  slot: string;
+};
 
-const AlertsFooter = ({slot}: PropTypes) => {
+const AlertsFooter = ({ slot }: PropTypes) => {
   const { anySelected, deleteSelectedAlerts } = useAlerts();
 
   return (
@@ -23,12 +27,16 @@ const AlertsFooter = ({slot}: PropTypes) => {
       <RuxButton
         secondary
         onClick={deleteSelectedAlerts}
-        style={{ marginRight: "1rem" }}
+        style={styles.dismissBtn}
         disabled={!anySelected}
       >
         Dismiss
       </RuxButton>
-      <RuxButton onClick={deleteSelectedAlerts} disabled={!anySelected}>
+      <RuxButton
+        onClick={deleteSelectedAlerts}
+        disabled={!anySelected}
+        style={{ paddingBottom: "1.5rem" }}
+      >
         Acknowledge
       </RuxButton>
     </footer>
