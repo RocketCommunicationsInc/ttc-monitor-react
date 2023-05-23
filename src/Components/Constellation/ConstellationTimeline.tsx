949af634063387a8ff47517bb31323116d72ce82
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-05-23 16:48:20 -0600

    updated scroll and fixed start and end time on timeline

--- src/Components/Constellation/ConstellationTimeline.tsx
+++ src/Components/Constellation/ConstellationTimeline.tsx
@@ -13,50 +13,44 @@ type PropTypes = {
 const ConstellationTimeline = ({ zoomLevel }: PropTypes) => {
   const { contacts, contactIds } = useContacts();
 
-  // const regions = [
-  //   "ALPH 6648",
-  //   "ECHO 1100",
-  //   "FOXI 3232",
-  //   "GOLF 5602",
-  //   "HOTL 0002",
-  //   "IRON 4090",
-  //   "JULI 3309",
-  //   "KILO 4684",
-  //   "LIMA 2017",
-  //   "MIKE 7509",
-  // ];
-
   return (
-    <RuxTimeline
-      timezone="America/New_York"
-      start="2023-05-23T00:09:00.000Z"
-      end="2023-05-24T18:00:09.695Z"
-      playhead="2023-05-23T04:00:00.000Z"
-      interval="hour"
-      zoom={zoomLevel}
-    >
-      {contactIds.map((contactId) => {
-        const beginDate = new Date(contacts[contactId].beginTimestamp).toISOString()
-          const endDate = new Date(contacts[contactId].endTimestamp).toISOString()
-        console.log(beginDate)
-        return (
-               <RuxTrack>
-          <div slot="label">{contacts[contactId].equipment.slice(0, 6)}</div>
-          <RuxTimeRegion
-            start={beginDate}
-            end={endDate}
-            status={contacts[contactId].status}
-          >
-            {contacts[contactId].satellite}
-          </RuxTimeRegion>
+    <div className="timeline-wrapper">
+      <RuxTimeline
+        timezone="UTC"
+        start="2023-05-23T11:00:00.000Z"
+        end="2023-05-24T11:00:00.005Z"
+        playhead="2023-05-23T14:00:00.000Z"
+        interval="hour"
+        zoom={zoomLevel}
+      >
+        {contactIds.map((contactId) => {
+          const beginDate = new Date(
+            contacts[contactId].beginTimestamp
+          ).toISOString();
+          const endDate = new Date(
+            contacts[contactId].endTimestamp
+          ).toISOString();
+          console.log(beginDate);
+          return (
+            <RuxTrack>
+              <div slot="label">
+                {contacts[contactId].satellite.slice(4, 10)}
+              </div>
+              <RuxTimeRegion
+                start={beginDate}
+                end={endDate}
+                status={contacts[contactId].status}
+              >
+                {contacts[contactId].satellite}
+              </RuxTimeRegion>
+            </RuxTrack>
+          );
+        })}
+        <RuxTrack slot="ruler">
+          <RuxRuler />
         </RuxTrack>
-        )
-      }
-      )}
-      <RuxTrack slot="ruler">
-        <RuxRuler />
-      </RuxTrack>
-    </RuxTimeline>
+      </RuxTimeline>
+    </div>
   );
 };
 
