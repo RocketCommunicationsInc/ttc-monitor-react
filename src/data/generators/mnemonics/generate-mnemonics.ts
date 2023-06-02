693366a03641365710f00bba5dc9c3487f5d54d7
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-06-02 11:49:11 -0700

    removes previous data generation

--- src/data/generators/mnemonics/generate-mnemonics.ts
+++
@@ -1,5 +0,0 @@
-import { generateMnemonic } from "./generate-mnemonic";
-
-export const generateMnemonics = (length: number = 40) => {
-  return Array.from({ length }, () => generateMnemonic());
-};
