8dba88d67edd1675bd2fc4ebee359fd70a9fed7e
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-06-20 16:36:07 -0600

    remove chart.js, install apex charts and begin styling and adding data to it

--- src/Components/Watcher/LineChart.tsx
+++ src/Components/Watcher/LineChart.tsx
@@ -1,46 +1,5 @@
-import {
-  Chart as ChartJS,
-  ChartType,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  SubTitle,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-import annotationPlugin from "chartjs-plugin-annotation";
 import type { Mnemonic } from "@astrouxds/mock-data";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  SubTitle,
-  Tooltip,
-  Legend,
-  annotationPlugin
-);
-
-const labels = [
-  "0800",
-  "0900",
-  "1000",
-  "1100",
-  "1200",
-  "1300",
-  "1400",
-  "1500",
-  "1600",
-];
-
-const tooltipTitle = () => {
-  return "";
-};
+import Chart from "react-apexcharts";
 
 type PropTypes = {
   data: Mnemonic;
@@ -48,160 +7,135 @@ type PropTypes = {
 };
 
 const LineChart = ({ data, chartData }: PropTypes) => {
-  const dataObj = {
-    labels,
-    datasets: [
-      {
-        label: "Value",
-        data: chartData,
-        borderColor: "rgb(77, 172, 255)",
-        pointRadius: 0,
-        gridLines: {
-          color: "#455D6E",
-          display: true,
-        },
-      },
-    ],
-  };
+  const labels = [
+    "0800",
+    "0900",
+    "1000",
+    "1100",
+    "1200",
+    "1300",
+    "1400",
+    "1500",
+    "1600",
+  ];
 
-  const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        top: 16,
-        left: 24,
-        right: 24,
-        bottom: 16,
+  var options = {
+    chart: {
+      stacked: false,
+      toolbar: {
+        show: false,
       },
     },
-    scales: {
-      x: {
-        fill: true,
-        border: {
-          display: true,
-          color: "white",
-        },
-        ticks: {
-          color: "white",
-        },
-        scaleLabel: {
-          display: true,
-        },
-      },
-      y: {
-        fill: true,
-        border: {
-          display: true,
-          color: "white",
-        },
-        grid: {
-          color: "grey",
-          drawTicks: false,
-        },
-        ticks: {
-          color: "white",
-          padding: 7,
-          stepSize: 10,
-        },
-        scaleLabel: {
-          display: true,
-        },
-        min: 0,
-        max: 110,
-        autoSkip: false,
+    title: {
+      text: "IRON 4090",
+      align: "left" as "left",
+      margin: 20,
+      offsetX: 50,
+      style: {
+        fontSize: "16px",
+        fontWeight: "bold",
+        color: "white",
       },
     },
-    plugins: {
-      legend: {
-        display: false,
+    subtitle: {
+      text: `${data.mnemonicId}`,
+      align: "left" as "left",
+      offsetX: 50,
+      style: {
+        fontSize: "12px",
+        color: "white",
       },
+    },
+    stroke: {
+      width: [3, 3],
+    },
+    xaxis: {
+      categories: labels,
       title: {
-        display: true,
-        text: "            IRON 4090",
-        color: "white",
-        align: "start",
-        font: {
-          size: 16,
-        },
-      },
-      subtitle: {
-        display: true,
-        text: `               ${data.mnemonicId}`,
-        color: "white",
-        align: "start",
-        font: {
-          size: 13,
-          weight: "normal",
-        },
-        padding: {
-          bottom: 25,
+        text: undefined,
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          color: "white",
+          fontSize: "12px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 600,
         },
       },
       tooltip: {
-        mode: "index",
-        intersect: false,
-        yAlign: "bottom",
-        displayColors: false,
-        titleMarginBottom: 0,
-        callbacks: {
-          title: tooltipTitle,
-        },
+        enabled: false,
       },
-      annotation: {
-        annotations: {
-          upperThreshold: {
-            type: "line" as ChartType,
-            yMin: 100,
-            yMax: 100,
-            borderColor: "white",
-            borderWidth: 2.5,
-            borderDash: [1, 2],
-            label: {
-              color: "#fff",
-              content: "Upper Limit",
-              display: true,
-              backgroundColor: "#172635",
-              opacity: 0.5,
-              font: {
-                size: 10.5,
-                weight: "normal",
-              },
-            },
+    },
+    yaxis: [
+      {
+        axisTicks: {
+          show: true,
+        },
+        axisBorder: {
+          show: true,
+          color: "white",
+        },
+        labels: {
+          style: {
+            colors: "white",
           },
-          lowerThreshold: {
-            type: "line" as ChartType,
-            yMin: 20,
-            yMax: 20,
-            borderColor: "white",
-            borderWidth: 2.5,
-            borderDash: [1, 2],
-            label: {
-              color: "#fff",
-              content: "Lower Limit",
-              display: true,
-              backgroundColor: "#172635",
-              font: {
-                size: 10.5,
-                weight: "normal",
-              },
-            },
+        },
+        title: {
+          style: {
+            color: "white",
           },
         },
       },
+      {
+        opposite: true,
+        axisBorder: {
+          show: true,
+          color: "white",
+        },
+      },
+    ],
+    tooltip: {
+      enabled: true,
+      // shared: true,
+      // followCursor: false,
+      // intersect: false,
+      // inverseOrder: false,
+      // fillSeriesColor: false,
+      // theme: false,
+      // style: {
+      //   fontSize: "12px",
+      //   backgroundColor: "black",
+      // },
+      // onDatasetHover: {
+      //   highlightDataSeries: false,
+      // },
+      // marker: {
+      //   show: true,
+      // },
+      // fixed: {
+      //   enabled: false,
+      //   position: "topRight",
+      //   offsetX: 0,
+      //   offsetY: 0,
+      // },
     },
-    hover: {
-      mode: "nearest",
-      intersect: false, //this is the point on the graph for tooltip
-    },
+    // legend: {
+    //   // horizontalAlign: "left" as "left",
+    //   offsetX: 40,
+    // },
   };
 
+  const series = [
+    {
+      name: "Line Chart",
+      data: chartData,
+    },
+  ];
+
   return (
-    <Line
-      // @ts-expect-error
-      options={options}
-      data={dataObj}
-    ></Line>
+    <div>
+      <Chart type="line" options={options} series={series} height="230%" />
+    </div>
   );
 };
 
